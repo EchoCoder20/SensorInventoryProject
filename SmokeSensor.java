f54e5905dc604e871a883af72f81fdced40a6506
EchoCoder20 <albusaidi1915@gmail.com>
2025-10-08 14:22:31 +0400

    Adding new object classes

---
+++ SmokeSensor.java
@@ -0,0 +1,20 @@
+public class SmokeSensor {
+    private Double smokeDetectionRange;
+    private String sensitivityLevel;
+
+    public Double getSmokeDetectionRange() {
+        return smokeDetectionRange;
+    }
+
+    public void setSmokeDetectionRange(Double smokeDetectionRange) {
+        this.smokeDetectionRange = smokeDetectionRange;
+    }
+
+    public String getSensitivityLevel() {
+        return sensitivityLevel;
+    }
+
+    public void setSensitivityLevel(String sensitivityLevel) {
+        this.sensitivityLevel = sensitivityLevel;
+    }
+}
